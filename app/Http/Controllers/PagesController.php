075c47db9e3512a50a24e098d00bd056fe8c05d7
MajdMaghamez <mm439@njit.edu>
2016-10-29 02:21:11 -0400

    Adding Controllers

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
-use App\Http\Requests;
+//use App\Http\Requests;
 
 class PagesController extends Controller
 {
-    //
+	public function home (){
+		return view ('home');
+	}
+	public function about (){
+		return view('about');
+	}
+	public function inventory (){
+		return view('inventory');
+	}
+	public function feedback (){
+		return view('feedback');
+	}
+	public function details (){
+		return view('details');
+	}
+	public function signup(){
+		return view('signup');
+	}
+	public function signin(){
+		return view('signin');
+	}
+	public function subscribe(){
+		return view('subscribe');
+	}
+	public function scheduler(){
+		return view('scheduler');
+	}
+	public function contactus(){
+		return view('contactus');
+	}
 }
