928461c6a441756fec6f805ac8a48820fb2485c7
ses10 <sesma@live.com>
2016-11-13 15:39:39 -0500

    add create car route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,17 +13,25 @@
 
 Route::get('/','PagesController@welcome');
 
-	Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
-	Route::post('/handleLogin', ['as' => 'handleLogin', 'uses' => 'AuthController@handleLogin']);
-	Route::get('/home', ['middleware' => 'auth', 'as' => 'home', 'uses' => 'UsersController@home']);
-	Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
-	Route::get('/create', ['as' => 'create', 'uses' => 'UsersController@create']);
-	Route::resource('users', 'UsersController', ['only' => ['create', 'store']]);
-	Route::get('/new-sales', ['as' => 'sales', 'uses' => 'SalesController@create']);
-	Route::resource('sales', 'SalesController', ['only' => ['create', 'store']]);
-	Route::get('/inventory','CarsController@index');
-	Route::get('/schedule','PagesController@schedule');
-	Route::get('/contact','PagesController@contact');
-	Route::get('/about','PagesController@about');
-	Route::get('/feedback','PagesController@feedback');
-	Route::get('/subscribe','PagesController@subscribe');
+/*** User Routes ***/
+Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
+Route::post('/handleLogin', ['as' => 'handleLogin', 'uses' => 'AuthController@handleLogin']);
+Route::get('/home', ['middleware' => 'auth', 'as' => 'home', 'uses' => 'UsersController@home']);
+Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
+Route::get('/create', ['as' => 'create', 'uses' => 'UsersController@create']);
+Route::resource('users', 'UsersController', ['only' => ['create', 'store']]);
+
+
+Route::get('/new-sales', ['as' => 'sales', 'uses' => 'SalesController@create']);
+Route::resource('sales', 'SalesController', ['only' => ['create', 'store']]);
+
+/*** Car Routes ***/
+Route::get('/car/create', ['as' => 'createCar', 'uses' => 'CarsController@create']);
+Route::get('/inventory','CarsController@index');
+	
+/*** Static Page Routes ***/
+Route::get('/schedule','PagesController@schedule');
+Route::get('/contact','PagesController@contact');
+Route::get('/about','PagesController@about');
+Route::get('/feedback','PagesController@feedback');
+Route::get('/subscribe','PagesController@subscribe');
